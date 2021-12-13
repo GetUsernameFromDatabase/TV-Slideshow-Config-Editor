ab58a59777f75f932e6d7069512d2b8769816ad9
Ryan Kruberg <rkruberg@gmail.com>
2021-12-13 18:55:16 +0200

    refactored parsing JSON

--- Components/FormMenu.cs
+++ Components/FormMenu.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json.Schema;
 using System;
 using System.IO;
 using System.Windows.Forms;
+using TV_Slideshow_Config_Editor.Logic;
 using TV_Slideshow_Config_Editor.ConfigInterface;
 
 namespace TV_Slideshow_Config_Editor
@@ -11,18 +12,12 @@ namespace TV_Slideshow_Config_Editor
     public partial class ConfigEditor : Form
     {
         public JSchema Schema { get; private set; } = JSchema.Parse(Properties.Resources.ConfigSchema);
-        readonly static public JsonSerializerSettings SerializerSettings = new JsonSerializerSettings
-        {
-            NullValueHandling = NullValueHandling.Ignore,
-            DateTimeZoneHandling = DateTimeZoneHandling.Utc,
-        };
         public Configurations Config { get; private set; }
         public string FilePath { get; private set; }
 
         private void SaveFile()
         {
-            var fileContent = JsonConvert.SerializeObject(this.Config,
-                Formatting.Indented, SerializerSettings);
+            var fileContent = ConfigJSON_Parser.Serialize(this.Config);
             using (StreamWriter file = new StreamWriter(this.FilePath))
                 file.Write(fileContent);
         }
@@ -39,10 +34,7 @@ namespace TV_Slideshow_Config_Editor
                     var fileContent = reader.ReadToEnd();
                     if (JObject.Parse(fileContent).IsValid(this.Schema))
                     {
-                        var JSON = JsonConvert.DeserializeObject<Configurations>(
-                            fileContent, SerializerSettings
-                        );
-                        this.Config = JSON;
+                        this.Config = ConfigJSON_Parser.DeSerialize(fileContent);
                         PopulatePages();
                     }
                     else MessageBox.Show("This is an improper TV Slideshow Configuration file",
@@ -53,9 +45,9 @@ namespace TV_Slideshow_Config_Editor
 
         private void MenuFile_New_Click(object sender, EventArgs e)
         {
+            this.FilePath = null;
             var source = Properties.Resources.DefaultJSON;
-            this.Config = JsonConvert.DeserializeObject<Configurations>(source,
-                SerializerSettings);
+            this.Config = ConfigJSON_Parser.DeSerialize(source);
             PopulatePages();
         }
 
---
+++ Logic/JSON_Parsing.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.RegularExpressions;
+using TV_Slideshow_Config_Editor.ConfigInterface;
+
+namespace TV_Slideshow_Config_Editor.Logic
+{
+    struct ConfigJSON_Parser
+    {
+        readonly static public JsonSerializerSettings SerializerSettings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+        };
+
+        public static string Serialize(Configurations Config)
+        {
+            return JsonConvert.SerializeObject(Config,
+               Formatting.Indented, SerializerSettings);
+        }
+
+        public static Configurations DeSerialize(string source)
+        {
+            return JsonConvert.DeserializeObject<Configurations>(
+                source, SerializerSettings); ;
+        }
+    }
+}
